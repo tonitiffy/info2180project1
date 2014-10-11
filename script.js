f88370133c601741933394b60ac2f4d4fa70f450
tonitiffy <toniannfitzgerald@gmail.com>
2014-10-11 12:58:07 +0000

    Third commit

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var contacts = [];
 
-
-
 function printPerson(person) {
     console.log(person.firstName + " " + person.lastName);
 }
@@ -30,4 +28,37 @@ function add(firstName, lastName, email, phoneNumber){
         phoneNumber: phoneNumber
     };
 }
- 
+
+function StaffMember(name,discountPercent){
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
+
+var cashRegister = {
+    total:0,
+    lastTransactionAmount: 0,
+    add: function(itemCost){
+        this.total += (itemCost || 0);
+        this.lastTransactionAmount = itemCost;
+    },
+    scan: function(item,quantity){
+        switch (item){
+        case "eggs": this.add(0.98 * quantity); break;
+        case "milk": this.add(1.23 * quantity); break;
+        case "magazine": this.add(4.99 * quantity); break;
+        case "chocolate": this.add(0.45 * quantity); break;
+        }
+        return true;
+    },
+    voidLastTransaction : function(){
+        this.total -= this.lastTransactionAmount;
+        this.lastTransactionAmount = 0;
+    },
+    // Create a new method applyStaffDiscount here
+    applyStaffDiscount: function(employee){
+        this.total = ((100-employee.discountPercent)/100)*this.total;
+    }
+    
+};
+
+console.log('Your bill is '+cashRegister.total.toFixed(2));
